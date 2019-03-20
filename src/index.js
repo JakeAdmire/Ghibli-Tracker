d27ac67852034ae182887770461f246749a88d1e
“Kimberly <kimberly.achkio@gmail.com>
2019-03-20 14:56:26 -0600

    Import createStore from redux
    Co-authored-by: Jake Admire <jakeadmire1@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './Components/App';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { rootReducer } from './reducers/index';
 import * as serviceWorker from './serviceWorker';
