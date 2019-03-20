47b0cf8b3c3333894d525b069a8e76cfa6705d31
“Kimberly <kimberly.achkio@gmail.com>
2019-03-20 14:31:55 -0600

    Update initial state to empty object
    Co-authored-by: Jake Admire <jakeadmire1@gmail.com>

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,12 @@
+export const userReducer = (state = {}, action) => {
+  switch(action.type) {
+
+    case('LOG_IN'):
+      return 
+      // action.user {email: '', password: ''}
+      // if ()
+
+    default:
+      return state;
+  }
+}
