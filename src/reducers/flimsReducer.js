597b89986ccc9510ca817eef411644db839846b0
“Kimberly <kimberly.achkio@gmail.com>
2019-03-20 16:03:33 -0600

    Build ADD_FILMS case within filmsReducer.js
    Co-authored-by: Jake Admire <jakeadmire1@gmail.com>

--- src/reducers/flimsReducer.js
+++ src/reducers/flimsReducer.js
@@ -1,6 +1,14 @@
 export const filmsReducer = (state = [], action) => {
   switch(action.type) {
 
+    case('ADD_FILMS'):
+      return [...state, {
+        id: action.id,
+        img: action.img,
+        title: action.title,
+        favorited: false
+      }]
+
     case('TOGGLE_FAVORITED'):
       return state.map(film => {
         if (film.id === action.id) {
