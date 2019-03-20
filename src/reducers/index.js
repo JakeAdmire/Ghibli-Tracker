6fbecf5ae9bd9625d4fd4fa72caa893b42552cb5
“Kimberly <kimberly.achkio@gmail.com>
2019-03-20 14:39:50 -0600

    Export rootReducer
    Co-authored-by: Jake Admire <jakeadmire1@gmail.com>

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ import { filmsReducer } from "./flimsReducer";
 import { filterReducer } from "./filterReducer";
 import { userReducer } from "./userReducer";
 
-const rootReducer = combineReducers ({
+export const rootReducer = combineReducers ({
   films: filmsReducer,
   filter: filterReducer,
   user: userReducer
