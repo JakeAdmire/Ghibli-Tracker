5d0b87a989cbb5a1e053eb1777089c0e7f1a2d98
“Kimberly <kimberly.achkio@gmail.com>
2019-03-20 14:40:32 -0600

    Create store within index.js
    Co-authored-by: Jake Admire <jakeadmire1@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './Components/App';
+import { Provider } from 'react-redux';
+import { rootReducer } from './reducers/index';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App /> 
+  </Provider>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
