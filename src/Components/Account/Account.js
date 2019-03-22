962ce52023012430b1b844334ed24de1ec8de275
Jake <jakeadmire1@gmail.com>
2019-03-22 11:50:00 -0600

    Build functionality to validate existing users
    Co-authored-by: Kim Myers <kimberly.achkio@gmail.com>

--- src/Components/Account/Account.js
+++ src/Components/Account/Account.js
@@ -16,18 +16,41 @@ export class Account extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state)
+    const { email, password } = this.state;
+    const userData = {
+      email: email.toLowerCase(),
+      password: password
+    } 
+    if (e.target.innerText === 'Log In') {
+      this.validateUser('/api/users', userData);
+    }
+  }
+
+  async validateUser(url = '', userData = {}) {
+    try {
+      const response = await fetch(`http://localhost:3000${url}`, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userData),
+      })
+      const data = await response.json();
+      console.log(data);
+    } catch(error) {
+        throw new Error('test');
+      }
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form>
         <label htmlFor='email'>Email:</label>
         <input type='text' name='email' value={this.state.email} onChange={this.handleChange} />
         <label htmlFor='password'>Password:</label>
         <input type='text' name='password' value={this.state.password} onChange={this.handleChange} />
-        <button>Log In</button>
-        <button>Sign Up</button>
+        <button onClick={this.handleSubmit}>Log In</button>
+        <button onClick={this.handleSubmit}>Sign Up</button>
       </form>
     )
   }
