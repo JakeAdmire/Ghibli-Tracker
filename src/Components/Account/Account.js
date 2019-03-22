738cd3749d42b20406153c3e86526e9e9f1d4250
Jake <jakeadmire1@gmail.com>
2019-03-22 09:27:24 -0600

    Build login form in render
    Co-authored-by: Kim Myers <kimberly.achkio@gmail.com>

--- src/Components/Account/Account.js
+++ src/Components/Account/Account.js
@@ -3,12 +3,32 @@ import React, { Component } from 'react';
 export class Account extends Component {
   constructor() {
     super();
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state)
   }
 
   render() {
     return (
-      <div>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor='email'>Email:</label>
+        <input type='text' name='email' value={this.state.email} onChange={this.handleChange} />
+        <label htmlFor='password'>Password:</label>
+        <input type='text' name='password' value={this.state.password} onChange={this.handleChange} />
+        <button>Log In</button>
+        <button>Sign Up</button>
+      </form>
     )
   }
 }
