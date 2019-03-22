b89fe4eadfbda504e205da00215e0e5b77136fa6
Jake <jakeadmire1@gmail.com>
2019-03-22 09:41:45 -0600

    Implement router into index.js
    Co-authored-by: Kim Myers <kimberly.achkio@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './Containers/App/App';
+import { BrowserRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { rootReducer } from './reducers/index';
@@ -13,7 +14,9 @@ const store = createStore(rootReducer, devTools);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App /> 
+    <BrowserRouter>
+      <App /> 
+    </BrowserRouter>
   </Provider>, 
   document.getElementById('root')
 );
