bb892bff0c7a1f0ea0145cba2ca1947a7c00c2c4
“Kimberly <kimberly.achkio@gmail.com>
2019-03-24 20:54:04 -0600

    Remove unnecessary code

--- src/Components/CardContainer/Card/Info/Info.js
+++ src/Components/CardContainer/Card/Info/Info.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 export function Info(props) {
-  //currently receiving props from app.js via mstp
-  console.log(props);
-
   return (
     <div>
       <h1>{props.title}</h1><p>{props.vote_average}/10</p>
