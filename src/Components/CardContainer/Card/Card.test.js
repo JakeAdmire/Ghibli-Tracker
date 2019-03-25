fa2cfe1ee6abe3448c4794197a012bcf04bfa151
“Kimberly <kimberly.achkio@gmail.com>
2019-03-24 20:00:46 -0600

    Write hook and default state test for card component

---
+++ src/Components/CardContainer/Card/Card.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Card } from './Card';
+
+describe('Card', () => {
+
+  let wrapper;
+  let key;
+  let mockInfo;
+
+  beforeEach(() => {
+    const mockId = 1
+    mockInfo = {poster: 'poster', title: 'title', id: mockId }
+    wrapper = shallow(<Card key={mockId} {...mockInfo}/>)
+  })
+
+  it('should have the correct default state', () => {
+    expect(wrapper.state()).toEqual({
+      favorite: false
+    })
+  })
+
+  it ('should match the snapshot', () => {
+
+  })
+
+})
