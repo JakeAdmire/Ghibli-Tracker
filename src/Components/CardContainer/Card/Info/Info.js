ae7c216d1b2fb2851cbc9ae323b6e9e13305c530
“Kimberly <kimberly.achkio@gmail.com>
2019-03-24 20:52:48 -0600

    Render individual movie information in Info.js component

--- src/Components/CardContainer/Card/Info/Info.js
+++ src/Components/CardContainer/Card/Info/Info.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 export function Info(props) {
+  //currently receiving props from app.js via mstp
   console.log(props);
+
   return (
     <div>
-      TEST
+      <h1>{props.title}</h1><p>{props.vote_average}/10</p>
+      <h2>{props.original_title}</h2>
+      <p>{props.release_date}</p>
+      <p>{props.overview}</p> 
     </div>
   )
 }
