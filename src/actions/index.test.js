faa3362840a26cc607a063eda0e44263aa0c628a
Jake <jakeadmire1@gmail.com>
2019-03-25 07:49:07 -0600

    Fix tests for user Action Creators

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -42,27 +42,15 @@ describe('actions', () => {
     expect(result).toEqual(expected)
   })
 
-  it('should return a type of ADD_USER', () => {
-    const email = 'test@test.com';
-    const password = 'password';
-    const expected = {
-      type: 'ADD_USER',
-      email,
-      password
-    }
-    const result = actions.addUser(email, password);
-    expect(result).toEqual(expected);
-  })
-
   it('should return a type of LOGIN_USER', () => {
-    const email = 'test@test.com';
-    const password = 'password';
+    const id = 1;
+    const name = 'Jake';
     const expected = {
       type: 'LOGIN_USER',
-      email,
-      password
+      id,
+      name
     }
-    const result = actions.loginUser(email, password);
+    const result = actions.loginUser(id, name);
     expect(result).toEqual(expected);
   })
   
