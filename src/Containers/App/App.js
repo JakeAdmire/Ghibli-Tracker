a9a260ce40b77722524a7ec119110bcccfd4e260
Jake <jakeadmire1@gmail.com>
2019-03-27 08:38:57 -0600

    Implement Loader functionality and fix router paths

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -9,13 +9,15 @@ import CardContainer from '../../Components/CardContainer/CardContainer';
 import { Header } from '../../Components/Header/Header';
 import { Info } from '../../Components/CardContainer/Card/Info/Info';
 import { addFilms } from '../../actions';
+import Loader from '../../Components/Loader/Loader';
 
 
 export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      redirect: false
+      redirect: false,
+      loading: false
     }
   }
 
@@ -24,9 +26,11 @@ export class App extends Component {
   }
 
   buildCards = async() => {
+    this.setState({loading: true});
     let url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_companies=10342`;
     const recentFilms = await help.fetchFilms(url);
     this.props.addFilms(recentFilms.results)
+    this.setState({loading: false});
   }
 
   determineFavorites = async () => {
@@ -37,27 +41,42 @@ export class App extends Component {
         this.buildCards();
       } else {
         this.props.addFilms(results.data);
+        this.setState({showFaves: true});
       }
     } else {
       this.setState({redirect: true})
     }
   }
 
+  countFavorites = async () => {
+    const { user } = this.props;
+    const results = user.name && await help.fetchFilms(`http://localhost:3000/api/users/${user.id}/favorites`);
+    return user.name ? 'FAVES' + '(' + results.data.length + ')' : 'FAVES(0)';
+  }
+
   render() {
+    const { user } = this.props;
+    let welcome = user.name ? `WELCOME ${user.name.toUpperCase()}` : 'LOG IN';
+    let faves = this.countFavorites();
+    console.log(faves);
     return (
       <div className="App">
         <Header />
-        <NavLink to='/login' className="login">LOG IN:</NavLink>
+        <NavLink to='/login' className="login">{welcome}</NavLink>
         <button className="show-faves" onClick={this.determineFavorites}>FAVES</button>
-        <Route exact path='/' component={CardContainer} />
+        {
+          this.state.loading ? <Loader /> : <Route exact path='/' component={CardContainer} />
+        }
         <Route exact path='/login' component={Account} />
-        <Route path='/:id' render={ ({match}) => {
+        <Route path='/movies/:id' render={ ({match}) => {
           const film = this.props.films.find(film => film.id == match.params.id);
           if (film) { return <Info {...film} /> }
         } }/>
-        <Route exact path='/' render={() => (
-          this.state.redirect && <Redirect to='/login' />
-        )} />
+        <Route exact path='/' render={() => {
+          if (this.state.redirect) return <Redirect to='/login' />
+          if (this.state.showFaves) return <Redirect to='/favorites' />
+        }} />
+        <Route exact path='/favorites' component={CardContainer} />
       </div>
     );
   }
@@ -77,5 +96,5 @@ export default connect(mapStateToProps, mapDispatchToProps)(App)
 App.propTypes = {
   addFilms: PropTypes.func.isRequired,
   films: PropTypes.array,
-  user: PropTypes.object.isRequired
+  user: PropTypes.object
 }
