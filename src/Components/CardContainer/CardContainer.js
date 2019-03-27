eff839f3322b1e345420841719121f1fe5f8f518
Jake <jakeadmire1@gmail.com>
2019-03-26 23:30:07 -0600

    Fix merge conflicts

--- src/Components/CardContainer/CardContainer.js
+++ src/Components/CardContainer/CardContainer.js
@@ -3,14 +3,9 @@ import Card from './Card/Card';
 import { connect } from 'react-redux';
 
 export function CardContainer(props) {
-
+  
   const { films } = props;
   const finalCards = films.map((film) => {
-    let info = {
-      poster: `https://image.tmdb.org/t/p/w500${film.poster_path}`,
-      title: film.title,
-      id: film.id
-    };
     return <Card key={film.id} {...film} />
   }) 
 
