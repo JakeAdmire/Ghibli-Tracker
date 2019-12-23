bbe46bcd135988c9a84c823fd0f46266bd710499
Jake <jakeadmire1@gmail.com>
2019-12-23 10:02:00 -0700

    Updates

---
+++ precache-manifest.6f3b9eff435ee0167211c1a93b21c2b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6b2fc58b8f23a3b17ff",
+    "url": "/ghibli-tracker/static/css/main.2ce096c7.chunk.css"
+  },
+  {
+    "revision": "f6b2fc58b8f23a3b17ff",
+    "url": "/ghibli-tracker/static/js/main.9066a1aa.chunk.js"
+  },
+  {
+    "revision": "aee7a20c7c6ea14766b2",
+    "url": "/ghibli-tracker/static/js/runtime~main.e27c783b.js"
+  },
+  {
+    "revision": "1f3fda394d9032ae055a",
+    "url": "/ghibli-tracker/static/js/2.9d2d5c3d.chunk.js"
+  },
+  {
+    "revision": "ff2179cbdbe3bda66241321bb2b1e395",
+    "url": "/ghibli-tracker/static/media/favorite.ff2179cb.svg"
+  },
+  {
+    "revision": "182041c2819b57dcc3b91df2fc7708a5",
+    "url": "/ghibli-tracker/index.html"
+  }
+];
